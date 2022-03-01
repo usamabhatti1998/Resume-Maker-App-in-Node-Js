25f86ba3cbff9c3bfd797c16d5612dbcaea98fe5
Usama Bhatti <47285459+usamabhatti1998@users.noreply.github.com>
2022-03-01 17:27:16 +0500

    Latest

---
+++ test.js
@@ -0,0 +1,12 @@
+var phantom = require('phantom');
+
+phantom.create().then(function(ph) {
+    ph.createPage().then(function(page) {
+        page.open("http://www.google.com").then(function(status) {
+            page.render('google.pdf').then(function() {
+                console.log('Page Rendered');
+                ph.exit();
+            });
+        });
+    });
+});
